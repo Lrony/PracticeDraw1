c2d8bcb847cc79d52d078d3fe63d7b89d5ee815c
pengyang <pengyang@pset.suntec.net>
2017-11-02 15:27:30 +0800

    Practice6DrawLineView
    Change-Id: I79f4a740c71f0fb2def4f3277c1083dfb1505b9b

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice6DrawLineView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice6DrawLineView.java
@@ -2,22 +2,32 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice6DrawLineView extends View {
 
+    private Paint m_paint = null;
+
     public Practice6DrawLineView(Context context) {
         super(context);
+        initPaint();
     }
 
     public Practice6DrawLineView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        initPaint();
     }
 
     public Practice6DrawLineView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initPaint();
+    }
+
+    private void initPaint() {
+        m_paint = new Paint();
     }
 
     @Override
@@ -25,5 +35,8 @@ public class Practice6DrawLineView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawLine() 方法画直线
+        m_paint.setAntiAlias(true);
+        m_paint.setStrokeWidth(5);
+        canvas.drawLine(100, 100, 300, 200, m_paint);
     }
 }
